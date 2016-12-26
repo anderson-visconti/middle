13139469db429d3803f6e6807056e566739aa4e4
anderson-visconti <anderson.svisconti@gmail.com>
2016-12-26 13:05:28 -0200

    v0.003 - Transformação para função

--- Previsao/Chuva.py
+++ Previsao/Chuva.py
@@ -1,71 +1,73 @@
-import pandas as pd
-import numpy as np
-from netCDF4 import *
-from datetime import datetime
+def pega_chuva(paths, nc_vars, coords, tempos):
+    import pandas as pd
+    import numpy as np
+    from netCDF4 import *
+    from datetime import datetime
 
-t1 = datetime.now()
-path = {'path_chuva': r'C:\OneDrive\Middle Office\Middle\Hidrologia\Chuva-Vazao\Chuva',
-        'path_export': r'C:\Users\ander\Desktop\Nova pasta'}
+    t1 = datetime.now()
+    path = {'path_chuva': r'C:\OneDrive\Middle Office\Middle\Hidrologia\Chuva-Vazao\Chuva',
+            'path_export': r'C:\Users\ander\Desktop\Nova pasta'}
 
-nomes = {'variavel': 'cmorph', 'base': 'cmorph.3hr-025deg.', 'extensao': '.nc'}
-nc_vars = {'chuva': 'cmorph_precip',
-           'lat': 'lat',
-           'lon': 'lon',
-           'tempo': 'time'}
+    nomes = {'variavel': 'cmorph', 'base': 'cmorph.3hr-025deg.', 'extensao': '.nc'}
+    nc_vars = {'chuva': 'cmorph_precip',
+               'lat': 'lat',
+               'lon': 'lon',
+               'tempo': 'time'}
 
-coords = {'lat': [-22, -20],
-          'lon': [313.4, 316]}
+    coords = {'lat': [-22, -20],
+              'lon': [313.4, 316]}
 
-tempos = {'t_inicial': '2016-01-20',
-          't_final': '2016-12-13'}
+    tempos = {'t_inicial': '2016-01-20',
+              't_final': '2016-12-13'}
 
-temp = pd.date_range(start=tempos['t_inicial'], end=tempos['t_final'], freq='D', tz='UTC')
-caminhos = []
+    temp = pd.date_range(start=tempos['t_inicial'], end=tempos['t_final'], freq='D', tz='UTC')
+    caminhos = []
 
-for i in range(0, len(temp)):   # cria caminhos dos arquivos
-    caminhos.append('{path}\{variavel}\{year}\{nome_base}{data_form}{extensao}'.format(
-        path=path['path_chuva'],
-        variavel=nomes['variavel'],
-        year=temp[i].year,
-        nome_base=nomes['base'],
-        data_form=temp[i].strftime('%Y%m%d'),
-        extensao=nomes['extensao']))
+    for i in range(0, len(temp)):   # cria caminhos dos arquivos
+        caminhos.append('{path}\{variavel}\{year}\{nome_base}{data_form}{extensao}'.format(
+            path=path['path_chuva'],
+            variavel=nomes['variavel'],
+            year=temp[i].year,
+            nome_base=nomes['base'],
+            data_form=temp[i].strftime('%Y%m%d'),
+            extensao=nomes['extensao']))
 
-file = MFDataset(caminhos)
-lats = file.variables[nc_vars['lat']][:]  # pega latitutes
-lons = file.variables[nc_vars['lon']][:]  # pega longitudes
+    file = MFDataset(caminhos)
+    lats = file.variables[nc_vars['lat']][:]  # pega latitutes
+    lons = file.variables[nc_vars['lon']][:]  # pega longitudes
 
-# indices para recortar em lat/lon
-lat_inds = np.where((lats >= coords['lat'][0]) & (lats <= coords['lat'][1]))
-lon_inds = np.where((lons >= coords['lon'][0]) & (lons <= coords['lon'][1]))
+    # indices para recortar em lat/lon
+    lat_inds = np.where((lats >= coords['lat'][0]) & (lats <= coords['lat'][1]))
+    lon_inds = np.where((lons >= coords['lon'][0]) & (lons <= coords['lon'][1]))
 
-# subset dos dados
-sub_lats = lats[lat_inds[0]]
-sub_lons = lons[lon_inds[0]]
-sub_times = num2date(file.variables[nc_vars['tempo']][:], file.variables[nc_vars['tempo']].units)
-precip = file.variables[nc_vars['chuva']][:, lat_inds[0], lon_inds[0]]
-dados = []
+    # subset dos dados
+    sub_lats = lats[lat_inds[0]]
+    sub_lons = lons[lon_inds[0]]
+    sub_times = num2date(file.variables[nc_vars['tempo']][:], file.variables[nc_vars['tempo']].units)
+    precip = file.variables[nc_vars['chuva']][:, lat_inds[0], lon_inds[0]]
+    dados = []
 
-for i in range(0, precip.shape[0]):  # tera sobre o tempo
-    for j in range(0, precip.shape[1]):  # itera sobre lat
-        for k in range(0, precip.shape[2]):  # itera sobre lon
-            dados.append([sub_times[i],
-                          sub_lats[j],
-                          sub_lons[k],
-                          precip[i, j, k] * 3])
+    for i in range(0, precip.shape[0]):  # tera sobre o tempo
+        for j in range(0, precip.shape[1]):  # itera sobre lat
+            for k in range(0, precip.shape[2]):  # itera sobre lon
+                dados.append([sub_times[i],
+                              sub_lats[j],
+                              sub_lons[k],
+                              precip[i, j, k] * 3])
 
-df = pd.DataFrame(data=dados, columns=['data_3h', 'lat', 'lon', 'precip_3h'])
-df_indexado = df.set_index(['data_3h', 'lat', 'lon'])
-# calcula chuva acumulada em 24 h
-df_24h = df_indexado.unstack(level=[2, 1]).resample('D').sum().stack(level=[2, 1])
-# renomeia colunas
-df_24h.index = df_24h.index.set_names('data', level=0)
-df_24h.rename(columns={'precip_3h': 'precip_24h'}, inplace=True)
-# monta dataframe tabular
-df_tabular = df_24h.unstack(level=[2, 1]).resample('D').sum()
-# esreve arquivos .csv
-# df_indexado.to_csv(r'{}\chuva-3.csv'.format(path_export), sep=';', decimal=',')
-df_24h.to_csv(r'{}\chuva-24.csv'.format(path['path_export']), sep=';', decimal=',')
-# df_tabular.to_csv(r'{}\chuva-tab.csv'.format(path_export), sep=';', decimal=',')
-print '{}: {}s'.format('Tempo total', (datetime.now() - t1).total_seconds())
-del df_24h, df, df_tabular, df_indexado , file
+    df = pd.DataFrame(data=dados, columns=['data_3h', 'lat', 'lon', 'precip_3h'])
+    df_indexado = df.set_index(['data_3h', 'lat', 'lon'])
+    # calcula chuva acumulada em 24 h
+    df_24h = df_indexado.unstack(level=[2, 1]).resample('D').sum().stack(level=[2, 1])
+    # renomeia colunas
+    df_24h.index = df_24h.index.set_names('data', level=0)
+    df_24h.rename(columns={'precip_3h': 'precip_24h'}, inplace=True)
+    # monta dataframe tabular
+    df_tabular = df_24h.unstack(level=[2, 1]).resample('D').sum()
+    # esreve arquivos .csv
+    # df_indexado.to_csv(r'{}\chuva-3.csv'.format(path_export), sep=';', decimal=',')
+    df_24h.to_csv(r'{}\chuva-24.csv'.format(path['path_export']), sep=';', decimal=',')
+    # df_tabular.to_csv(r'{}\chuva-tab.csv'.format(path_export), sep=';', decimal=',')
+    print '{}: {}s'.format('Tempo total', (datetime.now() - t1).total_seconds())
+    del df_24h, df, df_tabular, df_indexado , file
+    return df_24h, df_tabular
