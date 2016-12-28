25a742a9a26df7c32d3e0a4627d2c4106a598b02
anderson-visconti <anderson.svisconti@gmail.com>
2016-12-28 00:12:30 -0200

    v0.004 Transformação para função Descrição dos parametros

--- Previsao/Chuva.py
+++ Previsao/Chuva.py
@@ -1,6 +1,7 @@
-def pega_chuva(paths, nc_vars, coords, tempos):
+def pega_chuva(path, nomes, nc_vars, coords, tempos):
     '''
     :param paths: dicionario de caminhos
+    :param nomes: dicionario com estrutura da string dos arquivos netcdf
     :param nc_vars: dicionario com nomes das variaveis do arquivo netcdf
     :param coords: dicionario com lat/lon para subset dos dados
     :param tempos: dicionario com t_inicial e t_final para recorte dos dados
@@ -13,20 +14,20 @@ def pega_chuva(paths, nc_vars, coords, tempos):
     from datetime import datetime
 
     t1 = datetime.now()
-    path = {'path_chuva': r'C:\OneDrive\Middle Office\Middle\Hidrologia\Chuva-Vazao\Chuva',
-            'path_export': r'C:\Users\ander\Desktop\Nova pasta'}
-
-    nomes = {'variavel': 'cmorph', 'base': 'cmorph.3hr-025deg.', 'extensao': '.nc'}
-    nc_vars = {'chuva': 'cmorph_precip',
-               'lat': 'lat',
-               'lon': 'lon',
-               'tempo': 'time'}
-
-    coords = {'lat': [-22, -20],
-              'lon': [313.4, 316]}
-
-    tempos = {'t_inicial': '2016-01-20',
-              't_final': '2016-12-13'}
+    # path = {'path_chuva': r'C:\OneDrive\Middle Office\Middle\Hidrologia\Chuva-Vazao\Chuva',
+    #         'path_export': r'C:\Users\ander\Desktop\Nova pasta'}
+    #
+    # nomes = {'variavel': 'cmorph', 'base': 'cmorph.3hr-025deg.', 'extensao': '.nc'}
+    # nc_vars = {'chuva': 'cmorph_precip',
+    #            'lat': 'lat',
+    #            'lon': 'lon',
+    #            'tempo': 'time'}
+    #
+    # coords = {'lat': [-22, -20],
+    #           'lon': [313.4, 316]}
+    #
+    # tempos = {'t_inicial': '2016-01-20',
+    #           't_final': '2016-12-13'}
 
     temp = pd.date_range(start=tempos['t_inicial'], end=tempos['t_final'], freq='D', tz='UTC')
     caminhos = []
@@ -66,15 +67,78 @@ def pega_chuva(paths, nc_vars, coords, tempos):
     df = pd.DataFrame(data=dados, columns=['data_3h', 'lat', 'lon', 'precip_3h'])
     df_indexado = df.set_index(['data_3h', 'lat', 'lon'])
     # calcula chuva acumulada em 24 h
-    df_24h = df_indexado.unstack(level=[2, 1]).resample('D').sum().stack(level=[2, 1])
+    df_diaria = df_indexado.unstack(level=[2, 1]).resample('D').sum().stack(level=[2, 1])
     # renomeia colunas
-    df_24h.index = df_24h.index.set_names('data', level=0)
-    df_24h.rename(columns={'precip_3h': 'precip_24h'}, inplace=True)
+    df_diaria.index = df_diaria.index.set_names('data', level=0)
+    df_diaria.rename(columns={'precip_3h': 'precip_24h'}, inplace=True)
     # monta dataframe tabular
-    df_tabular = df_24h.unstack(level=[2, 1]).resample('D').sum()
-    # esreve arquivos .csv
+    df_tabular = df_diaria.unstack(level=[2, 1])
+
+    # escreve arquivos .csv
     # df_indexado.to_csv(r'{}\chuva-3.csv'.format(path_export), sep=';', decimal=',')
-    df_24h.to_csv(r'{}\chuva-24.csv'.format(path['path_export']), sep=';', decimal=',')
-    # df_tabular.to_csv(r'{}\chuva-tab.csv'.format(path_export), sep=';', decimal=',')
+    df_diaria.to_csv(r'{}\chuva-24.csv'.format(path['path_export']), sep=';', decimal=',')
+    df_tabular.to_csv(r'{}\chuva-tab.csv'.format(path['path_export']), sep=';', decimal=',')
     print '{}: {}s'.format('Tempo total para captura dos dados de chuva', (datetime.now() - t1).total_seconds())
-    return df_24h, df_tabular
+
+    return df_diaria, df_tabular
+
+def lags_chuva(df_tabular, n_lags):
+    '''
+
+    :param df_tabular: dataframe com dados de chuva em formato tabular
+    :param n_lags: dicionario contendo o range de lags para chuva
+    :return: dataframe com lags, sem remocao do NaN
+    '''
+    import numpy as np
+    import pandas as pd
+
+    df_temp = pd.DataFrame()    #  dataframe temporario
+
+    for i in np.arange(df_tabular.columns.shape[0]):  # loop para cada coluna
+
+        for j in range(n_lags['chuva'][0], n_lags['chuva'][1] + 1):  # loop para cada lag da variavel
+            df_temp = pd.concat(objs=[df_temp,
+                                      pd.Series(df_tabular[df_tabular.columns[i][0],
+                                                           df_tabular.columns[i][1],
+                                                           df_tabular.columns[i][2]],
+                                                name='lon_{lon}_lat_{lat}_lag{lag}'.
+                                                format(
+                                                    lon=df_tabular.columns[i][1],
+                                                    lat=df_tabular.columns[i][2],
+                                                    lag=j)
+                                                ).shift(j)
+                                      ],
+                                axis=1)
+
+    return df_temp
+
+def le_vazao(path, nomes_vazao, posto, tempos):
+    import pandas as pd
+    from datetime import datetime
+
+    t1 = datetime.now()
+    path = '{path_vazao}\{nome_vazao}{extensao}'.format(path_vazao=path['path_vazao'],
+                                                        nome_vazao=nomes_vazao['nome_vazao'],
+                                                        extensao=nomes_vazao['extensao']
+                                                        )
+
+    df_temp = pd.read_csv(filepath_or_buffer=path,
+                          sep=';',
+                          decimal=',',
+                          usecols=['Data', 'intCodPosto', 'VazaoNatural'],
+                          dtype={'Data': str},
+                          )
+
+    df_temp = df_temp.rename(columns={'Data': 'data', 'intCodPosto': 'posto', 'VazaoNatural': 'vazao_natural'})
+    df_temp = df_temp[
+        (df_temp['posto'] == posto['vazao']) |
+        (df_temp['posto'] == posto['montante'])
+        ]
+
+    df_temp['data'] = pd.to_datetime(df_temp['data'])
+    df_temp.set_index(['data', 'posto'], inplace=True)
+    df_temp = df_temp.unstack().resample('D').bfill()
+    df_temp = df_temp[tempos['t_inicial']: tempos['t_final']]
+
+    print 'Tempo total para captura dos dados de chuva: {}s'.format((datetime.now() - t1).total_seconds())
+    return df_temp
--- Previsao/main.py
+++ Previsao/main.py
@@ -1,24 +1,62 @@
 import pandas as pd
+import numpy as np
+from datetime import datetime
 from Chuva import *
+t1 = datetime.now()
 
+# Caminhos
 path = {'path_chuva': r'C:\OneDrive\Middle Office\Middle\Hidrologia\Chuva-Vazao\Chuva',
-            'path_export': r'C:\Users\ander\Desktop\Nova pasta'}
+        'path_export': r'C:\Users\ander\Desktop\Nova pasta',
+        'path_vazao': r'C:\Users\ander\Desktop'}
 
-nomes = {'variavel': 'cmorph', 'base': 'cmorph.3hr-025deg.', 'extensao': '.nc'}
+# Especificacoes do arquivo netcdf
+nomes = {'variavel': 'cmorph',
+         'base': 'cmorph.3hr-025deg.',
+         'extensao': '.nc'
+         }
+# Especificacoes do arquivo csv de vazoes
+nomes_vazao ={'nome_vazao': 'vazao',
+              'extensao': '.csv'}
+
+# Especificacoes de filtragem
+posto ={'vazao': 6,
+        'montante': 1}
+
 nc_vars = {'chuva': 'cmorph_precip',
            'lat': 'lat',
            'lon': 'lon',
-           'tempo': 'time'}
+           'tempo': 'time'
+           }
 
 coords = {'lat': [-22, -20],
-          'lon': [313.4, 316]}
+          'lon': [313.4, 316]
+          }
 
-tempos = {'t_inicial': '2016-01-20',
-          't_final': '2016-12-13'}
+tempos = {'t_inicial': '2016-01-01',
+          't_final': '2016-12-13'
+          }
 
-df_24h, df_tabular = pega_chuva(paths=path,
-                                nc_vars=nc_vars,
-                                coords=coords,
-                                tempos=tempos)
-print df_24h.head()
+# range de lags para cada variavel - chuva, vazao propria e vazao a montante
+n_lags = {'chuva': [0, 5],
+          'vazao': [1, 5],
+          'montante': [0, 7]
+          }
+
+# Pega dados de chuva
+df_24h, df_tabular = pega_chuva(path=path, nomes=nomes, nc_vars=nc_vars, coords=coords, tempos=tempos)
+
+# Pega dados de vazao montante
+df_vazao = le_vazao(path=path, nomes_vazao=nomes_vazao, posto=posto, tempos=tempos)
+
+# Prepara lags de chuva
+df_lags = lags_chuva(df_tabular=df_tabular, n_lags=n_lags)
+
+df_lags.to_csv(path_or_buf=r'{path_export}\chuva-lags.csv'.format(path_export=path['path_export']),
+               sep=';',
+               decimal=',')
+
+df_vazao.to_csv(path_or_buf=r'{path_export}\vazao.csv'.format(path_export=path['path_export']),
+               sep=';',
+               decimal=',')
+
 pass
