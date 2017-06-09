c621ffcc2b41279e09afcc419ce7d1073bb2c9a8
anderson-visconti <anderson.svisconti@gmail.com>
2017-06-09 09:36:18 -0300

    Add files via upload

--- CriacaoDecks/previvaz.py
+++ CriacaoDecks/previvaz.py
@@ -11,8 +11,9 @@ def worker(parametros):
     map(os.remove, glob.glob("*_fut.dat"))  # remove arquivo de previsao
 
     # Execucao do previvaz
+
     print ('Executando posto -> {:>3}'.format(parametros[0]))
-    os.system(r'{} > nul'.format(parametros[2]))
+    os.system(r'{} > NUL'.format(parametros[2]))
 
     # Captura dos dados de previsao
     file = open('{}\{}_fut.dat'.format(parametros[1], parametros[0]),mode='r').read().split('\n')
@@ -38,15 +39,121 @@ def le_banco(path):
     odbc_conn_str = r'DRIVER={Microsoft Access Driver (*.mdb)};UID=admin;UserCommitSync=Yes;Threads=3;SafeTransactions=0;PageTimeout=5;axScanRows=8;MaxBufferSize=2048;FIL={MS Access};DriverId=25;DefaultDir=C:\;DBQ=%s' %(os.path.join(path, 'Previvaz.mdb'))
 
     conn = pyodbc.connect(odbc_conn_str)
+    df_dados_gerais = pd.read_sql_query('select * from DadosGerais', conn)
+    df_opcoes = pd.read_sql('select * from DadosGerais', conn)
     df_dados_complemento = pd.read_sql_query('select * from DadosGeraisComplemento', conn)
     df_dados_semanais = pd.read_sql_query('select * from VazoesSemanais', conn)
+    df_limites = pd.read_sql_query('select * from LimitesPrevisao', conn)
+    df_postos = pd.read_sql_query('select * from Postos', conn)
+    return df_dados_gerais, df_opcoes, df_dados_semanais, df_dados_complemento, df_limites, df_postos
 
-    print df_dados_semanais.head()
 
+def cria_arquivos(parametros):
+    import os
+    import numpy as np
+    import codecs
+    import pandas as pd
 
-    return df_dados_semanais, df_dados_complemento
+    print 'Criacao de arquivos para o posto -> {}'.format(parametros['dados_config']['CodigoDoPosto'])
 
-def cria_arquivos(path, dados_complemento, dados_vazao):
+    #  cria pasta
+    path = r'{}\{}'.format(parametros['caso_export'], parametros['dados_config']['CodigoDoPosto'])
+
+    if os.path.exists(path=path) == False:
+        os.mkdir(path)
+
+    #  cria arquivo caso.dat
+    file_caso = file(os.path.join(path, 'caso.dat'), 'w')
+    file_caso.write('{}.inp'.format(parametros['dados_config']['CodigoDoPosto']))
+    file_caso.close()
+
+    #  cria arquivo .inp
+    file_inp = file(os.path.join(path, '{}.inp'.format(parametros['dados_config']['CodigoDoPosto'])), 'w')
+    file_inp.write(
+"""{}                       ! nome do arquivo de relatorio
+{}                       ! impressao dos parametros ?
+{}                       ! nome do arquivo que contem a serie total2
+{}                       ! sh sofrera transformacao? (0:nao; l:ln/nao; 2:bc/nao; 3:ln; 4:bc; 5:automatica)
+{}                      ! mes de inicio do an hidrologico
+{}
+{}                   ! nome do arquivo excel
+{}
+{}
+{}
+{}
+
+{}
+{}
+{}                       ! usa limite? (0:nao; 1:modelo e previsao; 2:apenas na previsao)
+{}                   ! nome do arquivo de limites
+{}                         0=>52 Semanas; 1 => 53 Semanas
+""".format(parametros['dados_gerais'].loc[parametros['index'], 'ArquivoRelatorio'],
+               'N',
+               parametros['dados_gerais'].loc[parametros['index'], 'ArquivoVazoesSemanais'],
+               parametros['dados_gerais'].loc[parametros['index'], 'Transformacao'],
+               parametros['dados_gerais'].loc[parametros['index'], 'MesInicial'],
+               parametros['dados_gerais'].loc[parametros['index'], 'ArquivoPrevisoes'],
+               parametros['dados_gerais'].loc[parametros['index'], 'ArquivoExcel'],
+               parametros['dados_gerais'].loc[parametros['index'], 'IntervaloConfianca'],
+               parametros['dados_config']['SemanaInicialPrev'],
+               parametros['dados_config']['AnoInicialPrev'],
+               1,
+               parametros['dados_config']['AnoFinalHistorico'],
+               parametros['dados_gerais'].loc[parametros['index'], 'CodigoDoPosto'],
+               parametros['dados_config']['FlagLimite'],
+               parametros['dados_config']['ArqLimite'],
+               parametros['dados_gerais'].loc[parametros['index'], 'CasoUsa53Semanas']
+               )
+    )
+    file_inp.close()
+
+    #  cria arquivo .lim
+    file_limite = file(os.path.join(path, '{}.lim'.format(parametros['dados_config']['CodigoDoPosto'])), 'w')
+    file_limite.write(
+'''{}          ! Agrupamento (1:semanal, 2:mensal, 3:trimestral, 4:semestral)
+{}          ! Faixas de Vazao (1:sem divisao, 2:duas faixas, 3:tres faixas, 4:quatro faixas)
+'''.format(parametros['dados_config']['Discretizacao'], parametros['dados_config']['Magnitude'])
+    )
+
+    for i in parametros['limites'].iterrows():
+        file_limite.write('{:>1d} {:>5.1f} {:>4.1f}\n'.format(int(i[1]['Ordem']),
+                                                        i[1]['LimiteInferior'],
+                                                        i[1]['LimiteSuperior']
+                                                        )
+
+    )
+
+    file_limite.close()
+
+    #  cria _str.dat
+    file_str = file(os.path.join(path, '{}_str.dat'.format(parametros['dados_config']['CodigoDoPosto'])), 'w', )
+    s =\
+'''      {posto:>3d}
+ {ano_inicial} {ano_final}  {drenagem}\n'''
+    file_str.write(s.format(posto=int(parametros['dados_config']['CodigoDoPosto']),
+                            ano_inicial=parametros['postos'].loc[parametros['index'] ,'AnoInicial'],
+                            ano_final=parametros['postos'].loc[parametros['index'] ,'AnoFinal'],
+                            drenagem=parametros['postos'].loc[parametros['index'] ,'AreaDeDrenagem'])
+                   )
+
+    for ano in pd.Series(parametros['dados']['Ano'].unique()).sort_values():
+
+        for i in range(1, 7):
+            linha = np.array(parametros['dados'].loc[(parametros['dados'].Semana >= 1 + 9 * (i - 1)) &
+                                            (parametros['dados'].Semana <= 9 * i) &
+                                            (parametros['dados'].Ano == ano)
+
+            ].Vazao).astype(int)
+
+            if len(linha) < 9:  #  completa com zeros as linha faltante
+                for k in range(9 - len(linha)):
+                    linha = np.append(linha, 0)
+
+            linha = np.append(linha, int(ano))
+            s = '  {0:>5}.  {1:>5}.  {2:>5}.  {3:>5}.  {4:>5}.  {5:>5}.  {6:>5}.  {7:>5}.  {8:>5}.    {9:>5}\n'
+            file_str.write(s.format(*linha))
+
+    file_str.close()
 
     return
 
@@ -61,6 +168,11 @@ if __name__ == '__main__':
     import pyodbc
 
     t1 = datetime.now()
+    print(r'Execucao em {} processos'.format(cpu_count()))
+    p = Pool(processes=cpu_count())
+    #p = Pool(processes=1)
+
+
     config_exec = {'path': r'C:\ENCAD3~1.0\Previvaz\bin\previvaz',
                    'exec': r'previvaz',
                    'licenca': r'C:\OneDrive\Middle Office\Middle',
@@ -69,34 +181,41 @@ if __name__ == '__main__':
                    'file_export': r'vazoes_previstas.csv'
                    }
 
-    path = {'caso': r'C:\Encad 3.0\usu_1\est_1\caso_1'
+    path = {#'caso': r'C:\Encad 3.0\usu_1\est_1\caso_1',
+            'caso': r'C:\Users\anderson.visconti\Desktop\teste',
+            'caso_export': r'C:\Users\anderson.visconti\Desktop\teste'
             }
 
+    df_dados_gerais, df_opcoes, df_dados_semanais, df_dados_complemento, df_limites, df_postos = le_banco(path['caso'])
+    dados_arquivos = []
 
-    #f_dados_semanais, df_dados_complemento = le_banco(path['caso'])
+    for index in df_dados_complemento.iterrows():
 
-    #print df_dados_complemento.head()
-    #print df_dados_semanais.head()
+        dic_aux = {'dados_gerais':df_dados_gerais[df_dados_gerais['CodigoDoPosto'] == index[1]['CodigoDoPosto']],
+                   'opcoes': df_opcoes[df_opcoes['CodigoDoPosto'] == index[1]['CodigoDoPosto']],
+                   'dados_config': index[1],
+                   'index':index[0],
+                   'dados': df_dados_semanais[df_dados_semanais['CodigoDoPosto'] == index[1]['CodigoDoPosto']],
+                   'limites': df_limites[df_limites['Codigo'] == index[1]['CodigoDoPosto']],
+                   'caso_export': path['caso_export'],
+                   'postos': df_postos[df_postos['CodigoDoPosto'] == index[1]['CodigoDoPosto']]
+                   }
 
-    #for index in df_dados_complemento.iterrows():
-    #    pass
-        #dic_aux = {'cod_posto': index[1]['CodigoDoPosto'],
-        #           'semana_inic': index[1]['SemanaInicialPrev'],
-        #           'ano_inic': index[1]['AnoInicialPrev'],
-        #           'Vazao':
+        dados_arquivos.append(dic_aux)
 
-        #}
-
+    result = p.map(func=cria_arquivos, iterable=dados_arquivos)
 
     folders = pd.Series(os.listdir(path['caso']))
     folders =  folders[(folders.isin(['Previvaz.mdb', 'Previvaz.ldb']) == False)].astype(int).sort_values()
     parametros = []
+
     for folder in folders.values:
         if folder not in  ['Previvaz.mdb', 'Previvaz.ldb']:
             aux = [int(folder),
                    r'{}\{}'.format(path['caso'], folder),
                    r'{}\{}'.format(config_exec['path'], config_exec['exec']),
-                   r'{}\{}'.format(config_exec['licenca'], config_exec['nome_lic'])
+                   r'{}\{}'.format(config_exec['licenca'], config_exec['nome_lic']),
+
             ]
 
         parametros.append(aux)
@@ -104,9 +223,8 @@ if __name__ == '__main__':
     parametros = np.array(parametros)
 
     # Paralelismo
-    #p = Pool(processes=1)
-    print('Execucao de previvaz em {} processos'.format(cpu_count()))
-    p = Pool(processes=cpu_count())
+    #print('Execucao de previvaz em {} processos'.format(cpu_count()))
+    #p = Pool(processes=cpu_count())
     result = p.map(func=worker, iterable=parametros)
     p.close()
 
