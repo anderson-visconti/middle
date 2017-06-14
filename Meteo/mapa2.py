01bd425ca9349bb3c0f906c9398f5bdafd8af261
anderson-visconti <anderson.svisconti@gmail.com>
2017-06-14 18:46:01 -0300

    Update mapa2.py

--- Meteo/mapa2.py
+++ Meteo/mapa2.py
@@ -3,22 +3,25 @@ def make_map():
     fig, ax = plt.subplots()
     m = Basemap(projection='merc',
                 llcrnrlat=-35,
-                urcrnrlat=12,
-                llcrnrlon=-85,
-                urcrnrlon=-30
+                urcrnrlat=9.0,
+                llcrnrlon=-78,
+                urcrnrlon=-30,
+                resolution='i'
                 )
     m.ax = ax
-    m.drawcoastlines()
-    m.drawcountries()
-    return fig, m
+    m.drawcoastlines(linewidth=0.15)
+    m.drawcountries(linewidth=0.15)
+    m.drawrivers(linewidth=0.3)
+    return m, ax
 
 
 def draw_states(ax, shapefile):
         shp = m.readshapefile(shapefile, 'states', drawbounds=True)
         for nshape, seg in enumerate(m.states):
-            poly = Polygon(seg, facecolor='1', edgecolor='k')
+            poly = Polygon(seg, facecolor='none', edgecolor='none', alpha=1.0)
             ax.add_patch(poly)
 
+        return
 
 if __name__ == '__main__':
     import pandas as pd
@@ -26,22 +29,18 @@ if __name__ == '__main__':
     from mpl_toolkits.basemap import Basemap
     import matplotlib.pyplot as plt
     from matplotlib.patches import Polygon
-    from matplotlib.colors import LinearSegmentedColormap
 
 
     df_teste = pd.read_fwf(
-        filepath_or_buffer=r'C:\Users\ander\Desktop\Eta40_precipitacao10d_11_06\ETA40_p110617a140617.dat',
+        filepath_or_buffer=r'C:\OneDrive\Middle Office\Middle\Hidrologia\Previsao de Vazao\Chuva\ETA\20170614\ETA40_p140617a150617.dat',
         delim_whitespace =True, decimal=',',
         names=['lon', 'lat', 'precip'], dtype={'lon':np.float, 'lat':np.float, 'precip':np.float})
 
     df_teste.head()
     lons, lats = df_teste.lon.unique(), df_teste.lat.unique()
     x, y = np.meshgrid(lons, lats)
+    df_teste['precip']  = df_teste['precip'].apply(lambda x: x * 100.0)
     precip = df_teste.pivot(index='lat', columns='lon', values='precip').values
-
-    print np.amin(lons), np.amax(lons)
-    print np.amin(lats), np.amax(lats)
-
     sub_set = {
         'llcrnrlat': float(np.amin(lats)),
         'urcrnrlat': float(np.amax(lats)),
@@ -49,28 +48,32 @@ if __name__ == '__main__':
         'urcrnrlon': float(np.amin(lons))
     }
 
-    for i in sub_set.values():
-        print i
-
-    fig, m = make_map()
-    m.drawcoastlines()
-    m.drawcountries()
-    m.drawstates()
-    m.drawrivers(linewidth=0.1)
+    m, ax = make_map()
+    draw_states(ax, shapefile=r'C:\OneDrive\Middle Office\Middle\Hidrologia\ShapeFiles\brasil\BRA_adm1')
     xx, yy = m(x, y)
-    levels = np.arange(0, 210, 10)
-    cores_ons = ['paleturquoise', 'mediumturquoise', 'dodgerblue',
-                 'steelblue', 'springgreen', 'limegreen', 'forestgreen',
-                 'yellow', 'gold', 'orangered', 'red', 'salmon', 'lightgray'
+    levels = [1, 5, 10, 15, 20, 25, 30, 40, 50, 75, 100, 150, 200]
+    cores_ons = [(255.0/255.0, 255.0/255.0, 255.0/255.0),
+                 (225.0/255.0, 255.0/255.0, 255.0/255.0),
+                 (180.0/255.0, 240.0/255.0, 250.0/255.0),
+                 (40.0/255.0, 130.0/255.0, 240.0/255.0),
+                 (20.0/255.0, 100.0/255.0, 210.0/255.0),
+                 (103.0/255.0, 254.0/255.0, 133.0/255.0),
+                 (24.0/255.0, 215.0/255.0, 6.0/255.0),
+                 (30.0/255.0, 180.0/255.0, 30.0/255.0),
+                 (255.0/255.0, 232.0/255.0, 120.0/255.0),
+                 (255.0/255.0, 192.0/255.0, 60.0/255.0),
+                 (255.0/255.0, 96.0/255.0, 0.0/255.0),
+                 (225.0/255.0, 20.0/255.0, 0.0/255.0),
+                 (251.0/255.0, 94.0/255.0, 107.0/255.0),
+                 (170.0/255.0, 170.0/255.0, 170.0/255.0)
                  ]
-
-    custom_map = LinearSegmentedColormap.from_list('my_cmap', colors=cores_ons)
-    plt.register_cmap(cmap=custom_map)
-    cs = m.contourf(xx, yy, precip, levels=levels, cmap='my_cmap', extend='both')
-    cbar = m.colorbar(cs, location='bottom')
+    plt.title('Modelo ETA')
+    cs = m.contourf(xx, yy, precip, levels=levels, colors=cores_ons, extend='both')
+    cbar = m.colorbar(cs, location='bottom', label='Preciptacao [mm]')
+    cbar.set_ticks(levels)
+    cbar.ax.tick_params(labelsize=8)
+    #plt.savefig(r'C:\OneDrive\Middle Office\Middle\Hidrologia\teste.png')
     plt.show()
 
 
     pass
-
-
